f89cdee2828ed134219b4d3976bf1e65392975a9
Paul Austen <pjaos@gmail.com>
2022-01-23 10:41:35 +0000

    - Add test email cmd line option. - Change the default inc backs from 56 to 94 (~ 4 months) to reduce disk space usage.

--- pbackup/backup.py
+++ pbackup/backup.py
@@ -116,6 +116,11 @@ class Backup(object):
 
         showCmdLine = self._options.show_cmd_line
 
+        if self._options.test_email:
+            if not self._options.email_server:
+                raise BackupError("To test the email you must define the email server.")
+            return
+
         #Load config is required to do so
         self._loadConfig()
 
@@ -822,6 +827,9 @@ class Backup(object):
 
             raise
 
+    def testEmail(self):
+        """@brief Send a test email"""
+        self._notifyEmail("Backup Email Test", body = "TESTING BACKUP EMAIL SEND\n\n\n" )
 
 def main():
     uo = UO()
@@ -837,12 +845,13 @@ def main():
     opts.add_option("--ssh",                    help="Followed by the src ssh host address (optional). If supplied this can include the username, E.G username@myserver (if no username is supplied the current username will be used). This may also include the SSH port number E.G username@myserver:22", default=None)
     opts.add_option("--log",                    help="Followed by the absolute path of the backup log file (optional).", default=None)
     opts.add_option("--max_full",               help="Followed by the maximum number of full backups to store (default=12). The defaults store 12 full backups and 31 incremental backups. With one backup a day this gives ~ 12 fullbackups a year with 31 incremental backups for each full backup (just over a years worth of history).", type="int", default=12)
-    opts.add_option("--max_inc",                help="Followed by the maximum number of incremental backups to store (default=56).", type="int", default=56)
+    opts.add_option("--max_inc",                help="Followed by the maximum number of incremental backups to store (default=94).", type="int", default=94)
 
     opts.add_option("--email_server",           help="Followed by the email (SMTP) server for notification of backup progress (optional). The SMTP server address can include the port number of the SMTP server (E.G smtp.gmail.com:587).", default=None)
     opts.add_option("--email_list",             help="Followed by a comma separated list of email addresses to be sent email notifications of backup progress (optional).", default=None)
     opts.add_option("--email_username",         help="Followed by the email username for notification of backup progress (optional).", default=None)
     opts.add_option("--email_password",         help="Followed by the email password for notification of backup progress (optional).", default=None)
+    opts.add_option("--test_email",             help="Send a test email to check the email works.", action="store_true")
 
     opts.add_option("--pre_script",             help="Followed by the absolute path of a script to be executed before the backup (optional). This is usefull is LVM snapshots are used. The script can be used to create the snapshot .", default=None)
     opts.add_option("--post_script",            help="Followed by the absolute path of a script to be executed after the backup (optional). This is usefull is LVM snapshots are used. The script can be used to remove the snapshot once the backup is complete.", default=None)
@@ -866,7 +875,10 @@ def main():
             uo.setLog(options.log)
 
         backup = Backup(uo, options)
-        backup.execute()
+        if options.test_email:
+            backup.testEmail()
+        else:
+            backup.execute()
 
     #If the program throws a system exit exception
     except SystemExit:
